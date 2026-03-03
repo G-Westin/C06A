b8f3ec6ccefd0a908ac9bd8b7f06319b7a12b22d
Gabriel Westin <gabpontes05@gmail.com>
2026-03-03 14:44:56 -0300

    Add files via upload

---
+++ projeto02/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        Zumbi z1 = new Zumbi();
+        Zumbi z2 = new Zumbi();
+
+        z1.nome = "Chris";
+        z2.nome = "Pizzozin";
+
+        z1.gritar();
+        z2.gritar();
+    }
+}
---
+++ projeto02/src/Zumbi.java
@@ -0,0 +1,8 @@
+public class Zumbi {
+    String nome;
+    float velocidade;
+
+    public void gritar(){
+        System.out.println("Zumbi esta gritando!!!");
+    }
+}
